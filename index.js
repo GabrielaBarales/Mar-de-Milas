d36481e15e56c1d407927523ad5ace377dac34df
antonellaag <tanagou92@gmail.com>
2022-09-29 22:14:45 -0300

    Arreglos
    - Inserción de lista
    - Cambios en el HTML y CSS
    - Se elimina la sección JS y se añade un script al final del HTML en su lugar

--- index.js
+++
@@ -1,6 +0,0 @@
-const navToggle = document.querySelector(".toggle");
-const container = document.querySelector(".menu");
-
-navToggle.addEventListener("click", () => {
-    container.classList.toggle("menu_visible");
-});
